70d6c5f3b798594e9c96bbe293d68155f7371ca8
jeanjoellevillanueva <jeanjoellevillanueva@github.com>
2024-03-25 20:28:17 +0800

    Created an example for selection sort

---
+++ selection_sort.py
@@ -0,0 +1,24 @@
+"""
+An example of `Bubble Sort Algorithm`.
+"""
+
+
+NUMBERS = [7, 12, 9, 11, 3]
+# Output: [3, 7, 9, 11, 12]
+
+
+def selection_sort(numbers:list):
+    """
+    Sort the array of integers from low to highest.
+    """
+    sorted_array = []
+    while len(numbers) != 0:
+        min_number = min(numbers)
+        idx = numbers.index(min_number)
+        sorted_array.append(numbers.pop(idx))
+    return sorted_array
+
+
+if __name__ == '__main__':
+    sorted_list = selection_sort(NUMBERS)
+    print(sorted_list)
